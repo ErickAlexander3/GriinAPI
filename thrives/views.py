9f84fa2893390c5d1827160043fdd52083c060e2
Erick <velasquez@paigesolutions.com>
2018-05-01 13:56:17 -0700

    small fix to force form view to send the csrf token (which was not working when deployed to server)

--- thrives/views.py
+++ thrives/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from django.views.generic import FormView
 from .models import ThriveForm
 
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.utils.decorators import method_decorator
+
+@method_decorator(ensure_csrf_cookie, name='dispatch')
 class TestView(FormView):
     template_name = 'form.html'
     form_class = ThriveForm
